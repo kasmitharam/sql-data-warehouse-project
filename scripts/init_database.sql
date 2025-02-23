dbc189d56343e790c9b23ced9b307da7979cc31b
kasmitharam <40885802+kasmitharam@users.noreply.github.com>
2025-02-23 06:22:19 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,28 @@
 
+
+USE master;
+GO
+
+  -- drop and recreate the database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+  BEGIN
+      ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+      DROP DATABASE DataWarehouse;
+END;
+GO
+  
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
