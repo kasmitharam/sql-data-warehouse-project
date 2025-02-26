bd47ec200450824aaea3688749641fefd95be0d4
kasmitharam <40885802+kasmitharam@users.noreply.github.com>
2025-02-26 06:13:33 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -17,6 +17,8 @@ cst_create_date DATE,
 dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+GO
+
 
 IF OBJECT_ID ('silver.crm_sales_details','U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details;
@@ -33,6 +35,7 @@ sls_price INT,
 dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+GO
 
 IF OBJECT_ID ('silver.crm_prd_info','U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
@@ -48,6 +51,8 @@ prd_end_dt DATE,
 dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+GO
+
 
 IF OBJECT_ID ('silver.erp_cust_az12','U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12;
@@ -58,6 +63,8 @@ GEN NVARCHAR(50),
 dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+GO
+
 IF OBJECT_ID ('silver.erp_loc_a101','U') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101;
 CREATE TABLE  silver.erp_loc_a101 (
@@ -65,6 +72,8 @@ CID	NVARCHAR(50),
 CNTRY NVARCHAR(50),
 dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+
+GO
 
 
 IF OBJECT_ID ('silver.erp_px_cat_g1v2','U') IS NOT NULL
